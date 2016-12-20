911f8cf7434b46e4e067a05cf1c158db2442ba3e
Steven Nunez <steven.nunez@gmail.com>
2016-12-20 18:55:29 -0500

    Some styles

---
+++ web/static/js/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import RoomList from './RoomList'
+import RoomDetails from './RoomDetails'
+
+const App = () => {
+  return (
+    <div id='app'>
+      <RoomList />
+      <RoomDetails />
+    </div>
+  )
+}
+
+export default App
